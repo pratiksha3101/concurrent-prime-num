d9ddbf808eb6fa011c6a75e6fa2bf3f535f90d26
Prateeksha <prateeksha1mail@gmail.com>
2025-02-24 23:04:04 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,12 +9,23 @@ import (
 func main() {
 	n := 100
 
-	numWorkers := 4
+	//numWorkers := 4
+	numWorkers := min(n, 4)
+
+	// chunkSize calculates the number of numbers each worker will handle.
+	// The formula (n + numWorkers - 1) / numWorkers ensures that the chunks are
+	// evenly distributed, and if there are leftover numbers, the last worker will
+	// handle the remaining numbers.
 	chunkSize := (n + numWorkers - 1) / numWorkers
 
 	var wg sync.WaitGroup
+
+	// results is a buffered channel that will hold maps of prime numbers found by each worker.
+	// The size of the buffer is set to numWorkers to avoid blocking.
+	//results := make(chan map[int]bool, numWorkers)
 	results := make(chan map[int]bool, numWorkers)
 
+	//wg.Add(numWorkers)
 	for i := 0; i < numWorkers; i++ {
 		start := i*chunkSize + 1
 		end := (i + 1) * chunkSize
@@ -26,12 +37,14 @@ func main() {
 		go worker(start, end, results, &wg)
 	}
 
-	// closing
+	// By using a separate goroutine to close the channel after the wg.Wait() ensures that the workers are done,
+	// you guarantee that the channel is closed only after all workers have completed their tasks.
 	go func() {
 		wg.Wait()
 		close(results)
 	}()
 
+	// collecting the prime numbers from each worker
 	allPrimes := make(map[int]bool)
 	for result := range results {
 		for num := range result {
@@ -67,6 +80,7 @@ func isPrime(n int) bool {
 }
 
 func worker(start, end int, results chan<- map[int]bool, wg *sync.WaitGroup) {
+	// It guarantees that wg.Done() will be called when the worker finishes, regardless of where the function exits.
 	defer wg.Done()
 	primes := make(map[int]bool)
 
